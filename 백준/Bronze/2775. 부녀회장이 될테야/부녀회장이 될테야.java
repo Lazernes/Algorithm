86e8cc36c7211094ec4651a40d2ec9519f13fa53
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2025-08-22 17:46:39 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,37 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int num(int k, int n) {
-        int[][] arr;
-        arr = new int[k + 1][n + 1];
+        int[][] D = new int[15][15];
 
-        for (int i = 0; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 0) {
-                    arr[i][j] = j;
-                } else if (j == 1) {
-                    arr[i][j] = 1;
-                } else {
-                    arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
-                }
+        // 배열 초기화
+        D[0][1] = 1;
+        for (int i = 1; i < 15; i++) {
+            D[0][i] = i;
+            D[i][1] = 1;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++) {
+                D[i][j] = D[i - 1][j] + D[i][j - 1];
             }
         }
 
-        return arr[k][n];
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-
+        int T = Integer.parseInt(bf.readLine());
         for (int i = 0; i < T; i++) {
-            int k = sc.nextInt();
-            int n = sc.nextInt();
-
-            System.out.println(num(k, n));
+            int k = Integer.parseInt(bf.readLine());
+            int n = Integer.parseInt(bf.readLine());
+            System.out.println(D[k][n]);
         }
     }
 }

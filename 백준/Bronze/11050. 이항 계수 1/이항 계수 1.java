bcb93adca07026465b4cbe2667a531e104a3e1a1
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2025-08-22 17:09:23 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,22 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
+        int[][] D = new int[11][11];
 
-        int result = 1;
-        int result_tmp = 1;
-
-        for (int i = 1; i <= K; i++) {
-            result = result * N;
-            N--;
-            result_tmp = result_tmp * i;
+        // 배열 초기화
+        for (int i = 0; i < 11; i++) {
+            D[i][1] = 1;
+            D[i][i] = 1;
+            D[i][0] = 1;
         }
 
-        System.out.println(result / result_tmp);
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            }
+        }
+
+        System.out.println(D[N][K]);
     }
 }

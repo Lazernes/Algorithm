9cfd4502f30e7c4323677284593c2b0f030ef0de
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2025-09-07 16:07:11 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,47 +1,23 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N + 1];
+        int[] D = new int[N + 1];
+        D[1] = 0;
 
-        if (N >= 2) {
-            arr[2] = 1;
-        }
-
-        if (N >= 3) {
-            arr[3] = 1;
-        }
-
-        for (int i = 4; i < N + 1; i++) {
-            int[] tmp = new int[3];
-
+        for (int i = 2; i <= N; i++) {
+            D[i] = D[i - 1] + 1;    // 1을 뺀다.
             if (i % 2 == 0) {
-                tmp[0] = arr[i / 2] + 1;
+                D[i] = Math.min(D[i], D[i / 2] + 1);
             }
             if (i % 3 == 0) {
-                tmp[1] = arr[i / 3] + 1;
+                D[i] = Math.min(D[i], D[i / 3] + 1);
             }
-            tmp[2] = arr[i - 1] + 1;
-
-            Arrays.sort(tmp);
-
-            for (int j = 0; j < 3; j++) {
-                if (tmp[j] != 0) {
-                    arr[i] = tmp[j];
-                    break;
-                }
-            }
-
         }
-
-        bw.write(arr[N] + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(D[N]);
     }
 }

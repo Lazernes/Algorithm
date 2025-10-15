2e31bcad91ae3e6f0004f021abba121c9576cfc6
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2025-10-16 00:07:49 +0900

    [Bronze II] Title: e 계산, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/6376. e 계산/e 계산.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("n e");
+        System.out.println("- -----------");
+        System.out.println("0 1");
+        System.out.println("1 2");
+        System.out.println("2 2.5");
+
+        double factorial = 2;
+        double i = 2;
+        double result = 2.5;
+
+        while (factorial < 9) {
+            factorial++;
+            i *= factorial;
+            result += 1 / i;
+            System.out.printf("%.0f %.9f\n", factorial, result);
+        }
+
+    }
+}

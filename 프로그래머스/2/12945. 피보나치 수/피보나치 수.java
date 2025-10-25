c1077ac625c2629a04526e0df6d7132880646779
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2025-10-26 00:19:52 +0900

    [level 2] Title: 피보나치 수, Time: 2.60 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] F = new int[100001];
+        F[0] = 0;
+        F[1] = 1;
+        
+        for(int i=2;i<n+1;i++) {
+            F[i] = (F[i - 1] + F[i - 2])%1234567;
+        }
+        
+        answer = F[n];
+        
+        return answer;
+    }
+}

6d1802273d23c4be57aabae811a4e1a3918c1a1b
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2025-10-27 22:53:34 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,27 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int count = oneCount(n);
+        int count = Integer.bitCount(n);
         
         while(answer == 0) {
-            if(oneCount(++n) == count) {
+            if(Integer.bitCount(++n) == count) {
                 answer = n;
             }
         }
         
         return answer;
-    }
-    
-    static int oneCount(int n) {
-        String BN = Integer.toBinaryString(n);
-        int count = 0;
-        
-        for(int i=0;i<BN.length();i++) {
-            if(BN.charAt(i) == '1'){
-                count++;
-            }
-        }
-        
-        return count;
     }
 }

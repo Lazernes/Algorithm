69e71c91caaebea6d5021235186d5dd56ca0e1f1
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2026-01-02 23:25:24 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 12000 * n + 2000 * k;
+        
+        while(n >= 10) {
+            answer -= 2000;
+            n -= 10;
+        }
+        
+        return answer;
+    }
+}

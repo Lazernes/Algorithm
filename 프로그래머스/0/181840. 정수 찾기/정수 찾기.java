41112d29fe9b0ec8883c656548fec9436cbd5d9c
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2026-01-25 23:31:43 +0900

    [level 0] Title: 정수 찾기, Time: 1.24 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        Arrays.sort(num_list);
+        if(Arrays.binarySearch(num_list, n) >=0) {
+            answer = 1;
+        }
+        return answer;
+    }
+}

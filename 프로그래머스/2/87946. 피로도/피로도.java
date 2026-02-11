bae98dce974a54fcee2c776ea0ac39496e7c0d8d
Myeongwoo <143478944+Lazernes@users.noreply.github.com>
2026-02-11 10:34:26 +0900

    [level 2] Title: 피로도, Time: 4.68 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    static private int max = 0;
+    static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        
+        dfs(0, k, dungeons);
+        
+        return max;
+    }
+    
+    static private void dfs(int depth, int k, int[][] dungeons) {
+        
+        for(int i=0; i<dungeons.length; i++) {
+            
+            if(!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
+                dfs(depth + 1, k - dungeons[i][1], dungeons);
+                visited[i] = false;
+            }
+        }
+        
+        max = Math.max(max, depth);
+    }
+}
